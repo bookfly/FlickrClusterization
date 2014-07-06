771d03753b0e0081a21e587bd763dd4a342e82a9
bookFly <jelenatjelena@gmail.com>
2014-07-06 15:52:42 +0200

    setting location

--- src/test/ReadingJson.java
+++ src/test/ReadingJson.java
@@ -81,7 +81,7 @@ public class ReadingJson {
             photo.setTitle("Reef");
         } else if (photo.getTitle().contains("Tiger Shark") || photo.getTitle().contains("Tiger shark") || photo.getTitle().contains("tiger shark")) {
             photo.setTitle("Tiger");
-        } else if (photo.getTitle().contains("White Shark") || photo.getTitle().contains("White shark") || photo.getTitle().contains("white shark") || photo.getTitle().contains("White_shark") || photo.getTitle().contains("white-shark") || photo.getTitle().contains("white_shark") || photo.getTitle().contains("White_Shark") || photo.getTitle().contains("Great white")|| photo.getTitle().contains("Great White") || photo.getTitle().contains("GREAT WHITE")) {
+        } else if (photo.getTitle().contains("White Shark") || photo.getTitle().contains("White shark") || photo.getTitle().contains("white shark") || photo.getTitle().contains("White_shark") || photo.getTitle().contains("white-shark") || photo.getTitle().contains("white_shark") || photo.getTitle().contains("White_Shark") || photo.getTitle().contains("Great white") || photo.getTitle().contains("Great White") || photo.getTitle().contains("GREAT WHITE")) {
             photo.setTitle("White");
         } else if (photo.getTitle().contains("Bull Shark") || photo.getTitle().contains("Bull shark") || photo.getTitle().contains("bull shark")) {
             photo.setTitle("Bull");
@@ -125,6 +125,127 @@ public class ReadingJson {
             photo.setTitle("Thresher");
         } else {
             photo.setTitle("Shark");
+        }
+
+    }
+
+    public void setLocation(Photo photo) {
+        if (photo.getLocation().contains("Australia")) {
+            photo.setLocation("Australia");
+        } else if (photo.getLocation().contains("USA")
+                || photo.getLocation().contains("US")
+                || photo.getLocation().contains("usa")
+                || photo.getLocation().contains(" United States of America")) {
+            photo.setLocation("USA");
+        } else if (photo.getLocation().contains("France")
+                || photo.getLocation().contains("france")
+                || photo.getLocation().contains("FRANCE")) {
+            photo.setLocation("France");
+        } else if (photo.getLocation().contains("New Zealand")) {
+            photo.setLocation("New Zealand");
+        } else if (photo.getLocation().contains("Kuwait") || photo.getLocation().contains("kuwait")) {
+            photo.setLocation("Kuwait");
+        } else if (photo.getLocation().contains("Hong Kong")) {
+            photo.setLocation("Hong Kong");
+        } else if (photo.getLocation().contains("Chicago")
+                || photo.getLocation().contains("Santa Barbara")
+                || photo.getLocation().contains("Seattle")
+                || photo.getLocation().contains("Pacific Grove")
+                || photo.getLocation().contains("Atlanta")
+                || photo.getLocation().contains("Tucson")
+                || photo.getLocation().contains("Honolulu")
+                || photo.getLocation().contains("Maui")
+                || photo.getLocation().contains("California")
+                || photo.getLocation().contains("Roaring Spring")
+                || photo.getLocation().contains("Maryland")
+                || photo.getLocation().contains("Savannah")
+                || photo.getLocation().contains("San Francisco")
+                || photo.getLocation().contains("Dallas")
+                || photo.getLocation().contains("Massachusetts")
+                || photo.getLocation().contains("Boston")) {
+            photo.setLocation("USA");
+        } else if (photo.getLocation().contains("Japan")) {
+            photo.setLocation("Japan");
+        } else if (photo.getLocation().contains("Canada")
+                || photo.getLocation().contains("canada")) {
+            photo.setLocation("Canada");
+        } else if (photo.getLocation().contains("THAILAND")
+                || photo.getLocation().contains("Thailand")) {
+            photo.setLocation("Thailand");
+        } else if (photo.getLocation().contains("Switzerland")
+                || photo.getLocation().contains("Schweiz")) {
+            photo.setLocation("Switzerland");
+        } else if (photo.getLocation().contains("Korea")) {
+            photo.setLocation("Korea");
+        } else if (photo.getLocation().contains("UK")) {
+            photo.setLocation("UK");
+        } else if (photo.getLocation().contains("Maldives")) {
+            photo.setLocation("Maldives");
+        } else if (photo.getLocation().contains("UK")
+                || photo.getLocation().contains("United Kingdom")
+                || photo.getLocation().contains("Scotland")
+                || photo.getLocation().contains("CORNWALL")
+                || photo.getLocation().contains("England")
+                || photo.getLocation().contains(" U.K.")
+                || photo.getLocation().contains("Britain")) {
+            photo.setLocation("UK");
+        } else if (photo.getLocation().contains("Taiwan")) {
+            photo.setLocation("Taiwan");
+        } else if (photo.getLocation().contains("netherlands")
+                || photo.getLocation().contains("Netherlands")) {
+            photo.setLocation("Netherlands");
+        } else if (photo.getLocation().contains("Pakistan")) {
+            photo.setLocation("Pakistan");
+        } else if (photo.getLocation().contains("Madalena do Pico")
+                || photo.getLocation().contains("Portugal")) {
+            photo.setLocation("Portual");
+        } else if (photo.getLocation().contains("Spain")
+                || photo.getLocation().contains("Coruña")
+                || photo.getLocation().contains("Palma de Mallorca")) {
+            photo.setLocation("Spain");
+        } else if (photo.getLocation().contains("Andorra")) {
+            photo.setLocation("Andorra");
+        } else if (photo.getLocation().contains("Malta")) {
+            photo.setLocation("Malta");
+        } else if (photo.getLocation().contains("Malaysia")) {
+            photo.setLocation("Malaysia");
+        } else if (photo.getLocation().contains("Belize")) {
+            photo.setLocation("Belize");
+        } else if (photo.getLocation().contains("México")
+                || photo.getLocation().contains("Mexico")) {
+            photo.setLocation("Mexico");
+        } else if (photo.getLocation().contains("Italia")) {
+            photo.setLocation("Italia");
+        } else if (photo.getLocation().contains("Maroc")) {
+            photo.setLocation("Maroco");
+        } else if (photo.getLocation().contains("Singapore")) {
+            photo.setLocation("Singapore");
+        } else if (photo.getLocation().contains("Greece")) {
+            photo.setLocation("Greece");
+        } else if (photo.getLocation().contains("Germany")
+                || photo.getLocation().contains("Deutschland")) {
+            photo.setLocation("Germany");
+        } else if (photo.getLocation().contains("South Africa")) {
+            photo.setLocation("South Africa");
+        } else if (photo.getLocation().contains("Shanghai")
+                || photo.getLocation().contains("China")) {
+            photo.setLocation("China");
+        } else if (photo.getLocation().contains("Sweden")) {
+            photo.setLocation("Sweden");
+        } else if (photo.getLocation().contains("Philippines")) {
+            photo.setLocation("Philippines");
+        } else if (photo.getLocation().contains("South Korea")) {
+            photo.setLocation("South Korea");
+        } else if (photo.getLocation().contains("Brasil")) {
+            photo.setLocation("Brasil");
+        } else if (photo.getLocation().contains("Russia")) {
+            photo.setLocation("Russia");
+        } else if (photo.getLocation().contains("Mozambique")) {
+            photo.setLocation("Mozambique");
+        } else if (photo.getLocation().contains(" Costa Rica")) {
+            photo.setLocation(" Costa Rica");
+        } else if (photo.getLocation().contains("Norway")) {
+            photo.setLocation("Norway");
         }
 
     }
