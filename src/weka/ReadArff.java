eadef97fa5d282ccc054e6453a115e88b3ea153c
bookFly <jelenatjelena@gmail.com>
2014-07-11 19:46:20 +0200

    visualization

--- src/weka/ReadArff.java
+++ src/weka/ReadArff.java
@@ -5,9 +5,12 @@
  */
 package weka;
 
+import java.awt.BorderLayout;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import weka.clusterers.ClusterEvaluation;
 import weka.clusterers.FilteredClusterer;
 import weka.clusterers.SimpleKMeans;
@@ -17,6 +20,9 @@ import weka.core.converters.ConverterUtils;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.AddCluster;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.gui.explorer.ClustererPanel;
+import weka.gui.visualize.PlotData2D;
+import weka.gui.visualize.VisualizePanel;
 
 /**
  *
@@ -52,12 +58,42 @@ public class ReadArff {
         eval.evaluateClusterer(data);
 
         writeFile(eval.clusterResultsToString(), txtName);
-        //  System.out.println(eval.clusterResultsToString());
+
+        // setup visualization
+        // taken from: ClustererPanel.startClusterer()
+        PlotData2D predData = ClustererPanel.setUpVisualizableInstances(data, eval);
+        String name = (new SimpleDateFormat("HH:mm:ss - ")).format(new Date());
+        String cname = filteredClusterer.getClass().getName();
+        if (cname.startsWith("weka.clusterers.")) {
+            name += cname.substring("weka.clusterers.".length());
+        } else {
+            name += cname;
+        }
+
+        VisualizePanel vp = new VisualizePanel();
+        vp.setName(name + " (" + data.relationName() + ")");
+        predData.setPlotName(name + " (" + data.relationName() + ")");
+        vp.addPlot(predData);
+
+        // display data
+        // taken from: ClustererPanel.visualizeClusterAssignments(VisualizePanel)
+        String plotName = vp.getName();
+        final javax.swing.JFrame jf
+                = new javax.swing.JFrame("Weka Clusterer Visualize: " + plotName);
+        jf.setSize(500, 400);
+        jf.getContentPane().setLayout(new BorderLayout());
+        jf.getContentPane().add(vp, BorderLayout.CENTER);
+        jf.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                jf.dispose();
+            }
+        });
+        jf.setVisible(true);
 
     }
 
-    private void writeClusters(Instances data, FilteredClusterer filteredClusterer) throws Exception {
 
+    private void writeClusters(Instances data, FilteredClusterer filteredClusterer) throws Exception {
         PrintWriter writer = new PrintWriter("data/resultWithClusters.txt");
         for (int i = 0; i < data.numInstances(); i++) {
             writer.print(data.instance(i).toString());
